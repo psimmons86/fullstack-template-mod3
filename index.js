f3d8d102f0a720fddd276412af32977b0758d40c
Patrick Simmons <hadroncollides@gmail.com>
2021-03-24 14:27:47 -0400

    just fetch

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Practice your JS here!
+const url = fetch('https://api.kanye.rest/')
+fetch('https://api.kanye.rest')
+.then(response => response.json())
+.then(function(quotes){
+    console.log(quotes)
+})
+
+function turnQuotesToText(quotes) {
+    console.log(quotes);
+    let QuotesLi = document.createElement('display');
+}
+
+document.append(QuotesLi)
+
+addEventListener
